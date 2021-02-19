6a6785d6f70b441df84405ecd2f1ec7e726b65d6
Priyanshu Kumar <priyanshupboy0786@gmail.com>
2021-02-20 00:20:06 +0530

    Component Update :wrench:

---
+++ widget/src/App.js
@@ -0,0 +1,26 @@
+import React from "react";
+import Accordion from "./components/Accordion";
+import Search from "./components/Search";
+
+const items = [
+  {
+    title: "What is React",
+    content: "React is a front end javascript framework",
+  },
+  {
+    title: "Why use React?",
+    content: "React is a favorite JS library among engineers",
+  },
+  {
+    title: "How do you use React?",
+    content: "React is a favorite JS library among engineers",
+  },
+];
+
+export default () => {
+  return (
+    <div>
+      <Search />
+    </div>
+  );
+};
---
+++ widget/src/components/Search.js
@@ -0,0 +1,47 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+
+const Search = () => {
+  const [term, setTerm] = useState("");
+  const [results, setResults] = useState([]);
+
+  console.log(results);
+
+  useEffect(() => {
+    const search = async () => {
+      const { data } = await axios.get("https://en.wikipedia.org/w/api.php", {
+        params: {
+          action: "query",
+          list: "search",
+          origin: "*",
+          format: "json",
+          srsearch: term,
+        },
+      });
+
+      setResults(data.query.search);
+    };
+
+    if (term) {
+      search();
+    }
+  }, [term]);
+
+  return (
+    <div>
+      <div className="ui form">
+        {" "}
+        <div className="field">
+          <label>Enter Search Term </label>
+          <input
+            value={term}
+            onChange={(e) => setTerm(e.target.value)}
+            className="input"
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Search;
