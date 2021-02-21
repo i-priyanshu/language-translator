e3565720156c6037826dc955b9aeb8ddb492f39f
Priyanshu Kumar <priyanshupboy0786@gmail.com>
2021-02-21 12:16:58 +0530

    added translator

--- widget/src/App.js
+++ widget/src/App.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import Accordion from "./components/Accordion";
 import Search from "./components/Search";
 import Dropdown from "./components/dropdown";
+import Translate from "./components/translate";
 
 const items = [
   {
@@ -34,21 +35,9 @@ const options = [
 ];
 
 export default () => {
-  const [selected, setSelected] = useState(options[0]);
-  const [showDropdown, setShowDropdown] = useState(true);
-
   return (
     <div>
-      <button onClick={() => setShowDropdown(!showDropdown)}>
-        Toggle Dropdown
-      </button>
-      {showDropdown ? (
-        <Dropdown
-          selected={selected}
-          onSelectedChange={setSelected}
-          options={options}
-        />
-      ) : null}
+      <Translate />
     </div>
   );
 };
---
+++ widget/src/components/translate.js
@@ -0,0 +1,52 @@
+import React, { useState } from "react";
+import Dropdown from "./dropdown";
+import Convert from "./convert";
+
+const options = [
+  {
+    label: "Afrikaans",
+    value: "af",
+  },
+  {
+    label: "Arabic",
+    value: "ar",
+  },
+  {
+    label: "Hindi",
+    value: "hi",
+  },
+  {
+    label: "Japanese",
+    value: "ja",
+  },
+  {
+    label: "german",
+    value: "de",
+  },
+];
+
+const Translate = () => {
+  const [language, setLanguage] = useState(options[0]);
+  const [text, setText] = useState("");
+  return (
+    <div>
+      <div className="ui form">
+        <div className="field">
+          <label>Enter Text</label>
+          <input value={text} onChange={(e) => setText(e.target.value)} />
+        </div>
+      </div>
+      <Dropdown
+        selected={language}
+        onSelectedChange={setLanguage}
+        options={options}
+        label="select a langauge"
+      />
+      <hr />
+      <h3 className="ui header">Output</h3>
+      <Convert text={text} language={language} />
+    </div>
+  );
+};
+
+export default Translate;
