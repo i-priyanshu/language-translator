b57c8fd0f7a0158f6f8103e53760764846af660a
Priyanshu Kumar <priyanshupboy0786@gmail.com>
2021-02-21 13:09:16 +0530

    added Navigation :mag:

--- widget/src/App.js
+++ widget/src/App.js
@@ -3,6 +3,8 @@ import Accordion from "./components/Accordion";
 import Search from "./components/Search";
 import Dropdown from "./components/dropdown";
 import Translate from "./components/translate";
+import Route from "./components/route";
+import Header from "./components/header";
 
 const items = [
   {
@@ -35,9 +37,28 @@ const options = [
 ];
 
 export default () => {
+  const [selected, setSelected] = useState(options[0]);
+
   return (
     <div>
-      <Translate />
+      <Header />
+      <Route path="/">
+        <Accordion items={items} />
+      </Route>
+      <Route path="/list">
+        <Search />
+      </Route>
+      <Route path="/dropdown">
+        <Dropdown
+          label="Select a color"
+          options={options}
+          selected={selected}
+          onSelectedChange={setSelected}
+        />
+      </Route>
+      <Route path="/translate">
+        <Translate />
+      </Route>
     </div>
   );
 };
